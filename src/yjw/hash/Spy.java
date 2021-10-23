864e04d8788f278d3aa155ae65fd8a15ec7adec0
jongwon.Yoo <ryu01410@gmail.com>
2021-10-23 17:31:29 +0900

    테스트 09.19

--- src/yjw/hash/Spy.java
+++ src/yjw/hash/Spy.java
@@ -5,9 +5,15 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * 공식 외우기
+ */
 public class Spy {
 
     public int solution(String[][] clothes){
+        /**
+         * 초기 값 : 1
+         */
         int answer = 1;
         Map<String, Integer> sortMap = new HashMap<>();
         for (String[] closet:clothes) {
@@ -16,11 +22,18 @@ public class Spy {
             sortMap.put(clothesDiv, sortMap.get(clothesDiv) + 1);
         }
 
+        /**
+         * 값 : 값 * 항목의 갯수 + 1
+         * 항목의 가짓 수 만큼 반복
+         */
         Iterator<Integer> it = sortMap.values().iterator();
         while(it.hasNext()) {
             answer *= it.next().intValue()+1;
         }
 
+        /**
+         * 리턴 값 : 값 -1
+         */
         return answer-1;
     }
 
