528b2700efb0cb60c72f7980d1d572750d54e26d
ryu01410@gmail.com <ryu01410@gmail.com>
2021-12-06 10:36:46 +0900

    연금복권 번호 생성기

---
+++ src/yjw/etc/PensionLottery.java
@@ -0,0 +1,26 @@
+package yjw.etc;
+
+
+import java.util.Random;
+
+/**
+ * 연금복권 번호 생성
+ * 192182
+ *
+ * 12/06 248487
+ *
+ */
+public class PensionLottery {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        random.setSeed(System.currentTimeMillis());
+        System.out.print(random.nextInt(10));
+        System.out.print(random.nextInt(10));
+        System.out.print(random.nextInt(10));
+        System.out.print(random.nextInt(10));
+        System.out.print(random.nextInt(10));
+        System.out.print(random.nextInt(10));
+    }
+
+}
