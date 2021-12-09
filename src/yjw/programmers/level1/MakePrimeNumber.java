62b0301cb9488403f63f53b5ebe4d585f93fb8f3
yjw8459 <ryu01410@gmail.com>
2021-12-09 09:52:31 +0900

    프로그래머스 - 소수 만들기

--- src/yjw/programmers/level1/MakePrimeNumber.java
+++ src/yjw/programmers/level1/MakePrimeNumber.java
@@ -1,9 +1,12 @@
 package yjw.programmers.level1;
 
+import java.util.ArrayList;
+
 /**
  * 소수 만들기
  * 문제 설명
- * 주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+ * 주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다. 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때,
+ * nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
  *
  * 제한사항
  * nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
@@ -25,10 +28,73 @@ package yjw.programmers.level1;
  */
 public class MakePrimeNumber {
 
-    public int solution(int[] nums) {
-        int answer = -1;
+    //내 답안
+    public int mySolution(int[] nums) {
+        int answer = 0;
 
+        for ( int i = 0; i < nums.length-2; i++ ){
+            for ( int j = i+1; j < nums.length-1; j++ ){
+                for ( int k = j+1; k < nums.length; k++ ){
+                    int target = nums[i] + nums[j] + nums[k];
+                    int check = 0;
+                    for ( int r = 2; r <= target/2; r++ ){
+                        if ( target % r == 0 ) {
+                            check = 1;
+                            break;
+                        }
+                    }
+                    if ( check == 0 ) answer++;
+                    check = 0;
+                }
+            }
+        }
+        System.out.println(answer);
         return answer;
     }
 
+    //다른사람 풀이
+    public int solution(int[] nums) {
+        int answer = 0;
+        boolean chk = false;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int num = nums[i] + nums[j] + nums[k]; //값을 담아
+                    if (num >= 2) //경우의 수를 찾아서
+                        chk = sosu(num);
+                    if (chk == true) //소수가 맞을 경우
+                        answer++;
+
+                }
+            }
+
+        }
+        return answer;
+    }
+
+    public boolean sosu(int num) {
+        boolean check = true;
+        if(num==2) { //소수일때
+            return check;
+
+        }
+        for(int i=2; i<num; i++) { //소수가 아닐때
+            if(num%i ==0) {
+                check = false;
+                break;
+            }
+        }
+        return check;
+    }
+
+    public static void main(String[] args) {
+        int[] test1 = { 1, 2, 3, 4 };
+        int[] test2 = { 1, 2, 7, 6, 4 };
+        MakePrimeNumber test = new MakePrimeNumber();
+        test.solution(test1);
+        test.solution(test2);
+    }
+
 }
+
