8c955208ca109c85c2a31ce56be476f002616d1b
yjw8459 <ryu01410@gmail.com>
2021-12-15 09:40:55 +0900

    백준 - 등수 매기기

--- src/yjw/backjun/Equalizer.java
+++ src/yjw/backjun/Equalizer.java
@@ -1,26 +1,34 @@
-package yjw;
+package yjw.backjun;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * 백준 - 등수매기기
+ *
+ *
+ */
+public class Equalizer {
 
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         ArrayList<Integer> list = new ArrayList<>();
-        long sum = 0;
+        int sum = 0;
         for ( int i = 0; i < n; i++ ){
             st = new StringTokenizer(br.readLine());
             list.add(Integer.parseInt(st.nextToken()));
             sum += (i+1);
         }
-        long max = list.stream().reduce(0, Integer::sum);
+        int max = list.stream().reduce(0, Integer::sum);
 
         System.out.println(sum - max);
         br.close();
     }
+
 }
